f88b335fc429661365494c1cb9280617862ed46e
Guilherme Barbosa Rogrigues <gui_tsuna@hotmail.comgit config --global user.email gui_tsuna@hotmail.comgit config --global user.email gui_tsuna@hotmail.com>
2022-01-24 09:34:42 -0300

    Loja de Games

---
+++ lojaDeGames/src/test/java/org/generation/lojaDeGames/LojaDeGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.lojaDeGames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaDeGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
