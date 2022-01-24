049c56b3eef235befd014fd080a5f84b4837ac4c
Guilherme Barbosa Rogrigues <gui_tsuna@hotmail.comgit config --global user.email gui_tsuna@hotmail.comgit config --global user.email gui_tsuna@hotmail.com>
2022-01-24 12:53:32 -0300

    @Quelann [ADD Controller]

---
+++ lojaDeGames/src/main/java/org/generation/lojaDeGames/Controller/lojaDeGamesController.java
@@ -0,0 +1,58 @@
+package org.generation.lojaDeGames.Controller;
+
+import java.util.List;
+
+/*
+ * @author Wesley Barreto Coelho
+ * @since 24/01/2022
+ * @version 0.01
+ */
+
+import org.generation.lojaDeGames.model.Categoria;
+import org.generation.lojaDeGames.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping("/categoria")
+@CrossOrigin("*")
+public class lojaDeGamesController {
+	@Autowired
+	private CategoriaRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Categoria>>GetAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping("/categoria/{id}")
+	public ResponseEntity<Categoria>findByGenero(@PathVariable (value = "id") long id){
+		return repository.findById(id)
+				.map(resp -> ResponseEntity.status(200).body(resp))
+				.orElseGet(() -> {
+					throw new ResponseStatusException(HttpStatus.NO_CONTENT, "ID não Encontrado");
+				});
+	}
+		
+	@GetMapping("/categoria/{genero}")
+	public ResponseEntity<List<Categoria>> getByGenero(@PathVariable String genero){
+		return ResponseEntity.ok(repository.findAllByGeneroContainingIgnoreCase(genero));
+	}
+		
+	@PostMapping("/save")
+	public ResponseEntity<Categoria>saveGenero(@RequestBody Categoria newCategoria){
+		return ResponseEntity.status(201).body(repository.save(newCategoria));
+	}
+
+}
+
+
