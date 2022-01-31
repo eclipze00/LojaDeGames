a2f4cca42cd824f51cd0e130434df1e77683e88e
Darllan <darllan13@hotmail.com>
2022-01-31 15:45:03 -0300

    @Darllan1L [TASK : add Controller]

---
+++ lojaDeGames/src/main/java/org/generation/lojaDeGames/Controller/UsuarioController.java
@@ -0,0 +1,42 @@
+package org.generation.lojaDeGames.Controller;
+
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.generation.lojaDeGames.model.UserLogin;
+import org.generation.lojaDeGames.model.Usuario;
+import org.generation.lojaDeGames.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
+@CrossOrigin(origins="*",allowedHeaders = "*")
+
+	public class UsuarioController {
+	
+	@Autowired
+		private UsuarioService usuarioService;
+	
+	@PostMapping("/logar")
+		public ResponseEntity<UserLogin>Authentication(@RequestBody Optional<UserLogin>user){
+		return usuarioService.Logar(user).map(resp-> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	} 
+	
+	@PostMapping("/cadastrar")
+		public ResponseEntity<Usuario>Post(@Valid @RequestBody Usuario usuario){
+		return ResponseEntity.status(201).body(usuarioService.CadastrarUsuario(usuario));
+		
+	}
+		
+	
+
+}
