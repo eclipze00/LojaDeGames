489e4b3b7edf7c0c0b9746384260e2416e3fec60
Wesley Barreto <wesley_barreto@outlook.com.br>
2022-01-31 11:33:14 -0300

    @Quelann [Task: add Service & Repository ]

--- lojaDeGames/src/main/java/org/generation/lojaDeGames/model/Usuario.java
+++ lojaDeGames/src/main/java/org/generation/lojaDeGames/model/Usuario.java
@@ -1,11 +1,15 @@
 package org.generation.lojaDeGames.model;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+@Entity
+@Table(name = "usuario")
 public class Usuario {
 	
 	@Id
