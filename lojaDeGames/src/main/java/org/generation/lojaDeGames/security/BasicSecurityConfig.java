b2b35a276bd64a575226ab99d9198a52834c4fae
Guilherme Barbosa Rogrigues <gui_tsuna@hotmail.com>
2022-02-03 15:37:45 -0300

    @eclipze00 [Task: Add Memory Authentication]

--- lojaDeGames/src/main/java/org/generation/lojaDeGames/security/BasicSecurityConfig.java
+++ lojaDeGames/src/main/java/org/generation/lojaDeGames/security/BasicSecurityConfig.java
@@ -23,11 +23,19 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired //o spring que vai gerenciar o método
 	private UserDetailsService userDetailsService;
 	
-	@Override //sobrescreve um método da classe "mãe"
+	//@Override //sobrescreve um método da classe "mãe"
 	//método que será chamado durante a autenticação
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception{ 
+	/*protected void configure(AuthenticationManagerBuilder auth) throws Exception{ 
 		auth.userDetailsService(userDetailsService);
-	}
+	}*/
+	
+	@Override
+ 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+ 		auth.userDetailsService(userDetailsService);
+
+		auth.inMemoryAuthentication().withUser("gameshop").password(passwordEnconder().encode("gameshop"))
+		 .authorities("ROLE_ADMIN");
+ 	}
 	
 	@Bean //para o Spring saber que vai gerenciar um método que vêm de uma classe importada(biblioteca)
 	public PasswordEncoder passwordEnconder() {
@@ -39,6 +47,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests()
 		.antMatchers("/usuario/logar").permitAll() //libera endpoints para que não seja preciso acessá-los com Token
 		.antMatchers("/usuario/cadastrar").permitAll()
+		.antMatchers("/usuario/atualizar").permitAll()
 		.anyRequest().authenticated() //todas as outras requesições precisaram de autenticação
 		.and().httpBasic() //utiliza o padrão Basic para gerar o Token
 		.and().sessionManagement() //indica o tipo de sessão que será utilizada
@@ -46,4 +55,6 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 		.and().cors() //habilita o cors -> Cross-Origin Resource Sharing
 		.and().csrf().disable(); // desabilita o Cross-site request forgery um dos ataques mais comuns da Web
 	}
+	
+	
 }
